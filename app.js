d6a31956c8989c5a688e11ebbb8bf475f129d316
Raul <raul010280@gmail.com>
2023-08-17 20:22:25 -0400

    all good

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const burgerButtom = document.querySelector(".nav-toggle");
 const inputBar = document.querySelector(".input-container");
 const navInfo = document.querySelector(".burger-buttom");
 const closeBtn = document.querySelector(".close-btn");
-
+const body = document.body;
 ////
 burgerButtom.addEventListener("click", function () {
   navInfo.style.display = "flex";
+  body.style.overflow = "hidden"
 });
 
 closeBtn.addEventListener("click", function () {
   navInfo.style.display = "none";
+  body.style.overflow = "auto"
 });
